5d3e748458e011e5d8b0d00c47f3778305cbea9d
Oleksii Zhmyrov <zhmyrov@gmail.com>
2013-08-28 23:56:09 +0300

    Render custom field Tester to cards

--- src/jira.py
+++ src/jira.py
@@ -2,15 +2,16 @@ import getpass
 import SOAPpy
 from config import *
 from classes import StoryCard
+import re
 
 soap = SOAPpy.WSDL.Proxy(JIRA_URL)
 
 
 def authorize():
     jira_user = raw_input("Username for jira: ")
-    if jira_user == "":
+    if jira_user == '':
         print 'JIRA login can not be empty string.'
-        exit
+        exit()
     password = getpass.getpass('Password for %s: (will not echo)' % jira_user)
 
     print 'Logging in to JIRA as {user}'.format(user=jira_user)
@@ -45,23 +46,41 @@ def get_stories_from_list(auth, list):
             project=PROJECT_NAME, key=story_number)
         story = soap.getIssuesFromJqlSearch(auth, search_string, SOAPpy.Types.intType(1))
 
-        story_points = 'n/a'
-        for i in range(5, 8):
-            if story[0][5][i].customfieldId in GREENHOPPER_CUSTOM_FIELDS:
-                story_points = story[0][5][i].values[0]
+        story_points = "n/a"
+        epic_key = None
+        tester = ""
+
+        node = story[0][5]
+        for i in range(1, len(node)+1):
+            try:
+                if node[i].customfieldId in GREENHOPPER_CUSTOM_FIELDS:
+                    story_points = node[i].values[0]
+                    continue
+                elif node[i].customfieldId in EPIC_NAME_CUSTOM_FIELDS:
+                    epic_key = node[i].values[0]
+                    continue
+                elif node[i].customfieldId in TESTER_CUSTOM_FIELDS:
+                    tester = node[i].values[0]
+                    continue
+                else:
+                    continue
+            except IndexError:
                 break
 
-        for i in range(1, 5):
-            if story[0][5][i].customfieldId in EPIC_NAME_CUSTOM_FIELDS:
-                epic_key = story[0][5][i].values[0]
-                break
-
-        search_string = 'project = {project} AND issuetype = epic AND key = {key}'.format(
-            project=PROJECT_NAME, key=epic_key)
-        epic = soap.getIssuesFromJqlSearch(auth, search_string, SOAPpy.Types.intType(1))
+        if epic_key is not None:
+            search_string = 'project = {project} AND issuetype = epic AND key = {key}'.format(
+                project=PROJECT_NAME, key=epic_key)
+            epic = soap.getIssuesFromJqlSearch(auth, search_string, SOAPpy.Types.intType(1))
+            epic_summary = epic[0].summary
+        else:
+            epic_summary = "n/a"
 
         description = story[0].description
-        description = (description, description[:128] + ' ...')[len(description) > 128]
+        description = (description, description[:127] + ' ...')[len(description) > 128]
+
+        assignee = (re.sub('_', ' ', str(story[0].assignee)), '')[story[0].assignee is None]
+        reporter = (re.sub('_', ' ', str(story[0].reporter)), '')[story[0].reporter is None]
+        tester = re.sub('_', ' ', str(tester))
 
         result.append(
             StoryCard(
@@ -69,11 +88,11 @@ def get_stories_from_list(auth, list):
                 number=story[0].key,
                 summary=story[0].summary,
                 description=description,
-                assignee=story[0].assignee,
-                tester="TBD",
-                reporter=story[0].reporter,
+                assignee=assignee,
+                tester=tester,
+                reporter=reporter,
                 points=story_points,
-                epic=epic[0].summary
+                epic=epic_summary
             )
         )
     return result
